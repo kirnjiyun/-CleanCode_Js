c1d5d9d1ea78c435180cc9de9801362ddbcc94a1
kirnjiyun <kimjiyunee@naver.com>
2023-12-07 23:10:19 +0900

    section3

---
+++ 타입다루기/타입검사.js
@@ -0,0 +1,42 @@
+function myFunction() {}
+class myClass {}
+
+typeof "문자열";
+typeof true;
+typeof undefined;
+typeof 123;
+typeof Symbol();
+typeof str;
+typeof null;
+typeof myFunction;
+typeof MyClass;
+//typeof가 만능은아니다
+//동적으로 변하는 언어라서 타입이 동적이다.
+//많은 주의가 필요하다.
+
+const p = {
+  name: "poco",
+  age: 99,
+};
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+const coco = new Person("poco", 99);
+
+poco instanceof Person; //true
+p instanceof Person; //false
+
+const arr = [];
+const func = function () {};
+const date = new Date();
+
+arr instanceof Array;
+func instanceof Function;
+date instanceof Date;
+arr instanceof Object;
+func instanceof Object;
+date instanceof Object;
+//다 true 결국 레퍼런스 타입이라 최상위는 다 오브젝트
+
+Object.prototype.toString.call(date); //[object Date]
